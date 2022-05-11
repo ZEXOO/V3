a543a8753ce456759759ae43a80d6c09669bee7e
ZEXXO <102238551+ZEXOO@users.noreply.github.com>
2022-05-11 22:54:58 +0700

    Update viapulsa.js

--- plugins/viapulsa.js
+++ plugins/viapulsa.js
@@ -1,11 +1,8 @@
 
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Dana
-╠➥ *6282361160044*
-╠➥ Chat Owner:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+*UNTUK PEMBAYARAN MINTA KE OWNER*
+Wa.me//6289668726356
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
