2411e24666ab01f36f2c6045c8eca87434a16cde
ZEXXO <102238551+ZEXOO@users.noreply.github.com>
2022-05-11 22:52:40 +0700

    Update viadana.js

--- plugins/viadana.js
+++ plugins/viadana.js
@@ -1,11 +1,7 @@
 let handler  = async (m, { conn, usedPrefix: _p }) => {
 let info = `
-╠═〘 PEMBAYARAN 〙 ═
-╠═ Untuk Pembayaran Via Gopay
-╠➥ *6287892711054*
-║- Owner Bot:
-║- wa.me/6287892711054
-╠═〘 Hyzer 〙 ═
+UNTUK PEMBAYARAN MINTA KE OWNER
+Wa.me//6289668726356
 `.trim()
 
 conn.fakeReply(m.chat, info, '0@s.whatsapp.net', '*Sewa Bot*', 'status@broadcast') 
