283b5c672fe73597e95bcf96e3f4c2210ee5839c
ZEXXO <102238551+ZEXOO@users.noreply.github.com>
2022-05-12 19:55:04 +0700

    Update sewabot.js

--- plugins/sewabot.js
+++ plugins/sewabot.js
@@ -1,32 +1,28 @@
 let fetch = require('node-fetch')
 let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
-    let kamisato = `
+    let ZEXXO BOTZ = `
 ╭═══ 〔 SEWA BOT 〕 ════
-├⬡ 𝟏 ᗰIᑎᘜᘜᑌ
-├⬡ 2 ᗰIᑎᘜᘜᑌ
-├⬡ 3 ᗰIᑎᘜᘜᑌ
-├⬡ 1 ᗷᑌᒪᗩᑎ
+├⬡ 1 HARI
+├⬡ 1 MINGGU
+├⬡ PERMANEN
+├⬡ 2GRUP PERMANEN
 └═══════════════
 𝗦𝗶𝗹𝗮𝗸𝗮𝗻 𝗸𝗹𝗶𝗸 𝗽𝗮𝗱𝗮 "𝗟𝗶𝘀𝘁 𝗛𝗮𝗿𝗴𝗮" 𝘂𝗻𝘁𝘂𝗸 𝗺𝗲𝗹𝗶𝗵𝗮𝘁 𝗹𝗶𝘀𝘁.
 ╭═══════════════════════
-║╭──❉ 〔 𝐕𝐈𝐀 𝐓𝐎𝐏𝐔𝐏 〕 ❉──────
-║│➸ *Digi* : 60149431385
-║│➸ *Umobile*: 60189830350
-║│➸ *Owner*: wa.me/6287892711054
+║╭──❉ 〔 SEWA CHAT OWNER 〕 ❉──────
+║│➸ *Owner*: Wa.me//6289668726356
 ║╰─────────────────────
 ╰═══════════════════════`.trim()
     const button = {
         buttonText: 'List Harga',
-        description: kamisato,
+        description: ZEXXO BOTZ,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '1 Minggu', description: "Rm5.00\nSewa bot 1 Minggu.", rowId:".masuk"},
-        {title: '2 Minggu', description: "Rm10.00\nSewa bot 2 Minggu.", rowId:".masuk"},
-        {title: '3 Minggu', description: "Rm15.00\nSewa bot 3 Minggu.", rowId:".masuk"},
-        {title: '1 Bulan', description: "Rm20.00\nSewa bot 1 Bulan.", rowId:".masuk"},
-        {title: 'Trial', description: "Free\nBot Free 1 Hari.", rowId:".masuk"},
+        {title: '1 HARI', description: "Rp3.00\nSewa bot 1 Minggu.", rowId:".masuk"},
+        {title: '1 Minggu', description: "Rp5.000\nSewa bot 2 Minggu.", rowId:".masuk"},
+        {title: 'PERMANEN', description: "Rp10.000\nSewa bot PERMANEN.", rowId:".masuk"},
+        {title: '2GRUP PERMANEN', description: "Rp15.000\nSewa bot 2GRUP PERMANEN.", rowId:".masuk"},
         {title: 'Owner', description: "Chat owner nya jika ada perlu.", rowId:".owner"},
-        {title: 'Rules', description: "Rules Bot LynXzy.", rowId:".snk"},
        ] }],
         listType: 1
        }
